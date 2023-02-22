8d4a928cc89c606eaaf031b8c120e2dfece6a20e
astroboy20 <tolulopeakinkunmi7@gmail.com>
2023-02-22 10:55:04 +0100

    todo-typescript

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import './TodoList.css'
+
+interface TodoProps{
+    items:{
+        id:string,
+        text:string
+        
+    }[]
+    onDeleteTodo: (id:string)=>void
+}
+const TodoList:React.FC<TodoProps> =(props)=>{
+    return(
+         <ul>
+           {props.items.map(item=>(
+            <li key={item.id}>
+               <span>{item.text}</span> 
+               <button onClick={props.onDeleteTodo.bind(null, item.id)}>DELETE</button>
+            </li>
+           ))}
+         </ul> 
+    )
+}
+
+export default TodoList
